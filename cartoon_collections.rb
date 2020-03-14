bd0237ea084841cb2e625093507dd2365d7e9b68
Isaac Smith <isaacsmith215@aol.com>
2020-03-14 17:20:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,9 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
